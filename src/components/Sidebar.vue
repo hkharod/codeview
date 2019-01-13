c4744cdd260165417deeb33c3bf1ae07ac16bd03
harbind <hkharod@gmail.com>
2019-01-12 23:36:15 -0500

    created dashboard route and sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dashboard">
+    <ul>
+    	<li>Project 1</li>
+    	<li>Project 2</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'sidebar'
+}
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dashboard">
+  	<div class="row">
+		<div class="col-3">
+			<sidebar></sidebar>
+		</div>
+		<div class="col-9">
+		</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+export default {
+  components: {
+  	Sidebar
+  },
+  name: 'dashboard'
+}
+
+</script>
