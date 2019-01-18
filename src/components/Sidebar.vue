fd651eb8533680a85d0bbc352e87c656878a2d5b
harbind <hkharod@gmail.com>
2019-01-17 23:53:43 -0500

    added projects check for sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,13 +5,15 @@
   			<i class="far fa-times-circle"></i>
   		</span>
   	</a>
-    
+    <div class="sidebar-logo">
+      <img src="/img/codeview.png"/>
+    </div>
     <div class="menu-container">
-      <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">+</i><span> New Project</span></a>
-      <ul>
-      	<li>Project 1</li>
-      	<li>Project 2</li>
+      <a class="btn-floating btn-large waves-effect waves-light cv-black"><i class="material-icons">+</i><span> New Project</span></a>
+      <ul v-if="projects.length">
+      	<li v-for="project in projects">{{ project.name }}</li>
       </ul>
+      <p v-if="!projects.length">You don't have any projects, click the button above to add one!</p>
     </div>
   </div>
 </template>
@@ -19,7 +21,12 @@
 <script>
 
 export default {
-  name: 'sidebar'
+  name: 'sidebar',
+  data() {
+    return {
+      projects: [],
+    }
+  }
 }
 
 </script>
@@ -31,5 +38,20 @@ export default {
     margin: 0 auto;
     padding-top: 20px;
     text-align: center;
+  }
+
+  .sidebar-logo {
+    border-bottom: 1px lightgrey solid;
+  }
+
+  .sidebar-logo img {
+    display: block;
+    margin: 0 auto;
+    width: 50%;
+    padding: 20px;
+  }
+
+  .cv-black {
+    background-color: #1B2B34;
   }
 </style>
